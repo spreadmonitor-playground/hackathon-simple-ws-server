c9c4f13e044b49f260de5f6c762847069cae2e99
Attila Olah <NoNameProvided@users.noreply.github.com>
2020-08-29 16:11:35 +0200

    fix: serve empty page on root for Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import { createServer } from 'http';
 import * as SocketIO from 'socket.io';
 
 import { connectionOptions } from './constants';
 import { LobbyHandler } from './lobby-handler.class';
 
-const server = SocketIO(connectionOptions);
+/** We need to add a basic HTTP server so HEROKU doesn't freak out when serving the base page. */
+const httpServer = createServer((req, res) => {
+  console.log(req.url === '/');
+  res.statusCode = req.url === '/' ? 200 : 404;
+  res.end('Nothing to see here.');
+});
+const socketServer = SocketIO(httpServer, connectionOptions);
 
 /** Init server logic for handling connections. */
-new LobbyHandler(server);
+new LobbyHandler(socketServer);
 
 /** Start listening and accepting connections. */
-server.listen(Number.parseInt(process.env['PORT'] as string) || 3000);
+httpServer.listen(Number.parseInt(process.env['PORT'] as string) || 3000);
